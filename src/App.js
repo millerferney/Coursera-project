49a17ef8d676ab536ff78d43af01cad116285b19
Miller Salcedo <millerfsalcedob@gmail.com>
2025-04-04 12:27:02 -0500

    First version changes

--- src/App.js
+++ src/App.js
@@ -1,27 +1,18 @@
-import { ChakraProvider } from "@chakra-ui/react";
-import Header from "./components/Header";
-import LandingSection from "./components/LandingSection";
-import ProjectsSection from "./components/ProjectsSection";
-import ContactMeSection from "./components/ContactMeSection";
-import Footer from "./components/Footer";
-import { AlertProvider } from "./context/alertContext";
-import Alert from "./components/Alert";
+import Homepage from './Homepage';
+import react from react;
+import './App.css';
 
 function App() {
-  return (
-    <ChakraProvider>
-      <AlertProvider>
-        <main>
-          <Header />
-          <LandingSection />
-          <ProjectsSection />
-          <ContactMeSection />
-          <Footer />
-          <Alert />
-        </main>
-      </AlertProvider>
-    </ChakraProvider>
-  );
+
+
+
+    return (
+        
+        <div>
+             <Homepage/>
+        </div>
+       
+    )
 }
 
 export default App;
